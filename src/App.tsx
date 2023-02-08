46ac163f46f1f14b0c1a7ee6c87e27ebdb8a7dfd
Marcin Pestka <pestka.m.j@gmail.com>
2023-02-08 19:27:40 +0100

    scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PorftolioComponent from './Components/portfolio';
@@ -6,16 +6,24 @@ import FirstProject from './Components/first_project';
 import SecondProject from './Components/second_project';
 import allProjects from './Components/all_Projects';
 import AllProjects from './Components/all_Projects';
+import NavBarCustom from './Components/navbar';
+
 
 function App() {
+  const ref = useRef<null | HTMLDivElement>(null);
+  const handleClick = () => {
+    ref.current?.scrollIntoView({behavior: 'smooth'});
+    console.log("test");
+  }
+
   return (
     <div className="App">
-      <a href="www.youtube.com">
-        <button>Posts</button>
-      </a>
+      <NavBarCustom handleClick={handleClick}></NavBarCustom>
       <PorftolioComponent></PorftolioComponent>
       <FirstProject></FirstProject>
+      <div ref={ref}>
       <SecondProject></SecondProject>
+      </div>
       <AllProjects></AllProjects>
     </div>
   );
