257b38d7855a2f736dd19d22e4662cf9b41e22ec
Marcin Pestka <pestka.m.j@gmail.com>
2023-02-14 10:19:43 +0100

    all_projects_links

--- src/Components/all_Projects.jsx
+++ src/Components/all_Projects.jsx
@@ -5,37 +5,56 @@ import 'semantic-ui-css/semantic.min.css';
   const itemsEN = [
     {
       header: 'Policy Issuing Tool',
+      link:"/en/policyautomation",
+      target:"",
       description:
         'My biggest commercial success, which serves daily in the operations of a large insurance agency.',
     },
     {
       header: 'Gdansk public transport site',
+      link:"https://github.com/MarcinPestka/public-transportation-portal",
+      target:"_blank",
       description:
         'A project that greatly benefits my daily personal life, I cannot imagine living without it.',
     },
     {
-      header: 'Pdf Generation Tool',
+      header: 'Data analysis automation tool',
+      link:"/en/dataanalysis",
+      target:"",
       description:
         'This is a project that taught me a lot about file creation and writing reliable code - generated files are sent to clients.',
     },
     {
       header: 'Book title copying nad formating',
+      link:"https://github.com/MarcinPestka/Library-site-chrome-extension",
+      target:"_blank",
       description:
         "This project was undertaken out of curiosity to explore the ease of writing a Chrome extension, resulting in a simple yet beneficial tool for writing my bachelor's thesis.",
     },
     {
       header: 'UI/UX policy handling site',
+      link:"",
+      target:"",
       description:
         "This project was created for the purpose of writing a bachelor's thesis and through it, I learned many useful aspects of UX/UI.",
     },
     {
       header: 'Portfolio page',
+      link:"https://github.com/MarcinPestka/portfolioapp",
+      target:"_blank",
       description:
         "Creating this website allowed me to enhance my UI/UX, Typescript, and React skills. I can confidently say I'm a Full-Stack developer.",
     },
+    {
+      header: 'Chat Gpt copy button',
+      link:"https://github.com/MarcinPestka/chatGPT-chrome-extension",
+      target:"_blank",
+      description:
+        "This project was undertaken out of curiosity to explore the ease of writing a Chrome extension, resulting in a simple yet beneficial tool for writing my bachelor's thesis.",
+    },
   ]
   const listItems = itemsEN.map((text) =>(
-    <Card href="https://linkedin.com" target='_blank'>
+    <Card href={text.link} target={text.target}>
       <Card.Content header={text.header} />
       <Card.Content description={text.description} />
     </Card>
